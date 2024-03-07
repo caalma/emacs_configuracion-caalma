67b95072a1a6eabc82e0417af6bc55b3ceabec2d
caalma <nds.suscript@gmail.com>
2024-03-06 22:55:10 -0300

    Ajuste

--- extras/instalar_firacode-linux.sh
+++ extras/instalar_firacode-linux.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-fonts_dir="${HOME}/.fonts/"
-mkdir -p "${fonts_dir}"
+fonts_dir="${HOME}/.fonts/FiraCode/"
+
 
 version=6.2
 zip=Fira_Code_v${version}.zip
+
+
+mkdir -p "${fonts_dir}"
+echo "Creada carpeta destino : ${fonts_dir}"
+
 curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
 unzip -o -q -d ${fonts_dir} ${zip}
-rm ${zip}
+echo "Descargados y descomprimidos los archivos de la fuente"
 
-echo "fc-cache -f"
+rm ${zip}
+rm -rf ${fonts_dir}woff
+rm -rf ${fonts_dir}woff2
+rm ${fonts_dir}fira_code.css
+rm ${fonts_dir}specimen.html
+echo "Borrados: paquete original, fuentes para web."
+
 fc-cache -f
+echo "Actualizado cache de tipografía"
